f83eac97514963723d6d4cc6fbcce4d9e7697569
Danielle Carrick <daniellecarrick@Danielles-MacBook-Air.local>
2017-02-24 19:14:08 +0200

    adding gitignore and also the basic functions work

---
+++ script.js
@@ -0,0 +1,24 @@
+var messages = [
+"No deal. Sad!",
+"I have the best words",
+"Terrific",
+"Build a wall",
+"I see bigly success in your future",
+"I have the best advice",
+"Build a wall"
+]
+
+var generateMessage = function() {
+  $('#messages').empty();
+  var upperLimit = messages.length - 1;
+  var index = Math.round(Math.random() * upperLimit);
+  console.log(messages[index]);
+  $('#messages').append(messages[index]);
+}
+
+
+$('button').on('click', function() {
+  generateMessage();
+})
+
+//generateMessage();
